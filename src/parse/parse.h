b931d74c39d25dcf1928528addc1f5bf3cb07965
yring-me <yangyt67@mail2.sysu.edu.cn>
2023-11-24 18:53:35 +0800

    移走一些声明

--- src/parse/parse.h
+++ src/parse/parse.h
@@ -10,11 +10,9 @@
 extern char args[10][128];
 
 void dispatcher();
-void redir_init();
-void clean_buffer();
+
 void parse_buffer(char *);
 void set_promat();
-void parse_redir();
 int check_cmd();
 
 #endif
