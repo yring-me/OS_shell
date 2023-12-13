269ed6a8dcbe931743424585414a7eb692941640
yring-me <yangyt67@mail2.sysu.edu.cn>
2023-12-14 01:41:30 +0800

    取消执行成功的输出

--- src/touch/touch.c
+++ src/touch/touch.c
@@ -1,13 +1,16 @@
 #include "touch.h"
 
-void syscall_touch(char *args0){
-    if(args0 == NULL){
+void syscall_touch(char *args0)
+{
+    if (args0 == NULL)
+    {
         printf("Use touch <file> to create file.");
         return;
     }
     const char *filename = args0;
     FILE *file = fopen(filename, "a");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error touch file");
         return;
     }
@@ -15,10 +18,11 @@ void syscall_touch(char *args0){
 
     struct utimbuf new_times;
     new_times.actime = new_times.modtime = time(NULL);
-    if (utime(filename, &new_times) == -1) {
+    if (utime(filename, &new_times) == -1)
+    {
         printf("Error updating file timestamp");
         return;
     }
 
-    printf("File '%s' created successfully.\n", filename);
+    // printf("File '%s' created successfully.\n", filename);
 }
